b05573300d5c011e7211fd40526ddd0bedc6dbb1
claudiodantas <claudiodantas1996@gmail.com>
2025-05-25 18:37:54 -0300

    Update user roles to use 'VISITOR' instead of 'USER' and adjust corresponding enum values

--- types/user.ts
+++ types/user.ts
@@ -1,15 +1,15 @@
 export enum Role {
-  USER = 'user',
-  ADMIN = 'ADMIN',
-  ATTENDANT = 'atendente',
-  COORDINATOR = 'coordenador',
+  VISITOR = "visitante",
+  ADMIN = "ADMIN",
+  ATTENDANT = "atendente",
+  COORDINATOR = "coordenador",
 }
 
 export interface User {
-  fullname: string
-  document: string
-  phoneNumber: string
-  email: string
-  password: string
-  role?: Role
+  fullname: string;
+  document: string;
+  phoneNumber: string;
+  email: string;
+  password: string;
+  role?: Role;
 }
